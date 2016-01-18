9412e0f750650026fa893ad9c8194b336aee9bd0
JeremyBec <jeremy.bec.pro@gmail.com>
2016-01-18 10:19:22 +0100

    Initial Commit

---
+++ CoreGraphicsTutorial/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreGraphicsTutorial
+//
+//  Created by Jeremy Bec on 18/01/2016.
+//  Copyright © 2016 JeremyBec. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
